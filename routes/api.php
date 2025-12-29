45ae05c05771560ede4e7e8021a3d4a84be6cd44
Zenilton Pereira <zeniltonp0@gmail.com>
2025-12-29 10:47:03 -0300

    criando rota e controller de register

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/register', [RegisterController::class, 'register'])->name('register');
