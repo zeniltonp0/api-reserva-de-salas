b22e7a63a8aa38f744dd7ee6b961b1a43102852e
Zenilton Pereira <zeniltonp0@gmail.com>
2025-12-31 14:49:27 -0300

    Adicionando relacionamentos nos Models

--- app/Models/Sala.php
+++ app/Models/Sala.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Sala extends Model
 {
     /** @use HasFactory<\Database\Factories\SalaFactory> */
     use HasFactory;
+
+    public function agendamentos(): HasMany {
+        return $this->hasMany(Agendamento::class);
+    }
 }
