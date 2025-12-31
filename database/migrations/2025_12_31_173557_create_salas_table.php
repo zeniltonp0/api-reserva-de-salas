f18547f04c91955db68950c402babe70c87632d3
Zenilton Pereira <zeniltonp0@gmail.com>
2025-12-31 14:45:21 -0300

    Migration de Sala e Model, Factory e Seeder de Agendamento

--- database/migrations/2025_12_31_173557_create_salas_table.php
+++ database/migrations/2025_12_31_173557_create_salas_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('salas', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->integer('capacidade');
+            $table->integer('andar');
+            $table->boolean('ativa');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2025_12_31_174231_create_agendamentos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Sala;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agendamentos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'user_id')->constrained();
+            $table->foreignIdFor(Sala::class, 'sala_id')->constrained();
+            $table->dateTime('inicio');
+            $table->dateTime('fim');
+            $table->text('motivo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agendamentos');
+    }
+};
