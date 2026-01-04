285dcf7926dd201e37dd16a05f7914dfafcb0b54
Zenilton Pereira <zeniltonp0@gmail.com>
2026-01-04 10:26:18 -0300

    Padronizando o formato do json de Agendamento com AgendamentoResource

--- app/Http/Controllers/Agendamento/StoreController.php
+++ app/Http/Controllers/Agendamento/StoreController.php
@@ -6,18 +6,16 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Agendamento\StoreAgendamentoRequest;
+use App\Http\Resources\Agendamento\AgendamentoResource;
 use Illuminate\Support\Facades\Auth;
 
 class StoreController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(StoreAgendamentoRequest $request)
     {
         $user = Auth::user();
 
-        $user->agendamentos()->create([
+        $agendamento = $user->agendamentos()->create([
             'sala_id' => $request->sala_id,
             'inicio' => $request->inicio,
             'fim' => $request->fim,
@@ -25,9 +23,6 @@ class StoreController extends Controller
             'status_id' => 1
         ]);
 
-        return response()->json([
-            'message' => 'Agendamento criado com sucesso!',
-            'data' => $request->inicio,
-        ], 201);
+        return AgendamentoResource::make($agendamento);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 12345678,
-            'role' => 'estudante'
+            'role' => 'admin'
         ]);
 
         $this->call([
